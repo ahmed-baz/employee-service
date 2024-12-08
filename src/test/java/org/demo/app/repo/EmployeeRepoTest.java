8d5b8533d3829cc517a9fab58999c804b7f944ab
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-08 19:15:34 +0200

    - update EmployeeIntegrationTest

--- src/test/java/org/demo/app/repo/EmployeeRepoTest.java
+++ src/test/java/org/demo/app/repo/EmployeeRepoTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -18,6 +19,7 @@ import java.util.Optional;
 @DataJpaTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Configuration
 class EmployeeRepoTest {
 
     @Autowired
