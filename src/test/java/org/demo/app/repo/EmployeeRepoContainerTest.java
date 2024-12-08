83111df731a20d2588e0e313482c2cc8853aee11
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-08 17:12:01 +0200

    - update EmployeeRepoMockTest - create EmployeeRepoContainerTest

---
+++ src/test/java/org/demo/app/repo/EmployeeRepoContainerTest.java
@@ -0,0 +1,57 @@
+package org.demo.app.repo;
+
+
+import org.assertj.core.api.Assertions;
+import org.demo.app.model.EmployeeEntity;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Optional;
+
+@Testcontainers
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class EmployeeRepoContainerTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
+
+    @Test
+    @Order(1)
+    void testSaveEmployee() {
+        EmployeeEntity employee = EmployeeEntity.builder()
+                .firstName("Ahmed")
+                .lastName("Ali")
+                .email("ahmed.ali@tesla.com")
+                .salary(new BigDecimal(10000))
+                .joinDate(new Date())
+                .build();
+
+        EmployeeEntity savedEmployee = employeeRepo.save(employee);
+        Assertions.assertThat(savedEmployee).isNotNull();
+        Assertions.assertThat(savedEmployee.getEmail()).isEqualTo("ahmed.ali@tesla.com");
+
+    }
+
+    @Test
+    @Order(2)
+    void testFindByEmail() {
+        Optional<EmployeeEntity> employeeEntity = employeeRepo.findByEmail("ahmed.ali@tesla.com");
+        Assertions.assertThat(employeeEntity.isPresent()).isTrue();
+    }
+
+}
--- src/test/java/org/demo/app/repo/EmployeeRepoMockTest.java
+++ src/test/java/org/demo/app/repo/EmployeeRepoMockTest.java
@@ -4,15 +4,8 @@ package org.demo.app.repo;
 import org.assertj.core.api.Assertions;
 import org.demo.app.model.EmployeeEntity;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -20,14 +13,9 @@ import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 
-@Testcontainers
-@SpringBootTest
-@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@RunWith(SpringRunner.class)
 class EmployeeRepoMockTest {
-
-    @Container
-    @ServiceConnection
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
     @MockBean
     private EmployeeRepo employeeRepo;
