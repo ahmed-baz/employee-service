4f4808e4857ec56a3a7dd3732dfa7d234edd2fbe
Ahmed Baz <ahmedbaz1024@gmail.com>
2024-12-09 16:27:59 +0200

    allow API cross origin

--- src/main/java/org/demo/app/controller/EmployeeController.java
+++ src/main/java/org/demo/app/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/employees")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class EmployeeController {
 
     private final EmployeeService employeeService;
